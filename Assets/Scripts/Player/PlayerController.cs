4112139c2a9dd1bb0aec93971b046c878636276b
duc150704 <nvduc150704@gmail.com>
2025-06-12 21:38:37 +0700

    update

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public TileClass selectedTile;
+    public int selectedSlotIndex = 0;
+    public GameObject hotbarSelector;
+
+    public GameObject handHolder;
+
+    public Inventory inventory;
+    public bool inventoryShowing = false;
+
+    public ItemClass selectedItem;
 
     public int playerRange;
     public Vector2Int mousePos;
-    
+
+
     public float moveSpeed;
     public float jumpForce;
     public bool onGround;
@@ -24,41 +33,25 @@ public class PlayerController : MonoBehaviour
     public TerrainGeneration terrainGenerator;
     private void Awake()
     {
+        terrainGenerator = FindObjectOfType<TerrainGeneration>();
+    }
+    private void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        inventory = GetComponent<Inventory>();
     }
 
     public void Spawn()
     {
         GetComponent<Transform>().position = spawnPos;
     }
-    private void OnTriggerStay2D(Collider2D col)
-    {
-        if (col.CompareTag("Ground"))
-            onGround = true;
-    }
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.CompareTag("Ground"))
-            onGround = false;
-    }
-    private void FixedUpdate()
+    private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float jump = Input.GetAxis("Jump");
         float vertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(horizontal * moveSpeed, rb.velocity.y);
-
-        hit = Input.GetMouseButton(0);
-        place = Input.GetMouseButton(1);
-
-        if (Vector2.Distance(transform.position, mousePos) <= playerRange && Vector2.Distance(transform.position, mousePos) > 0.7f)
-        {
-            if (hit)
-                terrainGenerator.RemoveTile(mousePos.x, mousePos.y);
-            else if (place)
-                terrainGenerator.CheckTile(selectedTile, mousePos.x, mousePos.y, false);
-        }
 
         if (horizontal > 0)
             transform.localScale = new Vector3(-1, 1, 1);
@@ -71,12 +64,78 @@ public class PlayerController : MonoBehaviour
                 movement.y = jumpForce;
         }
         rb.velocity = movement;
-        animator.SetFloat("Walk", Mathf.Abs(horizontal));
-        animator.SetBool("hit", hit || place);
-    }
-    private void Update()
-    {
+
+        hit = Input.GetMouseButton(0);
+        place = Input.GetMouseButton(1);
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            //cuon len
+            if (selectedSlotIndex < inventory.inventoryWidth - 1)
+                selectedSlotIndex += 1;
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            //cuon xuong
+            if (selectedSlotIndex > 0)
+                selectedSlotIndex -= 1;
+        }
+        //thiet lap o dc chon UI
+        hotbarSelector.transform.position = inventory.hotbarUISlots[selectedSlotIndex].transform.position;
+        if (selectedItem != null)
+        {
+            handHolder.GetComponent<SpriteRenderer>().sprite = selectedItem.sprite;
+            if (selectedItem.itemType == ItemClass.ItemType.block)
+                handHolder.transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+            else
+                handHolder.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            handHolder.GetComponent<SpriteRenderer>().sprite = null;
+        }
+
+        //thiet lap item chon
+        if (inventory.inventorySlots[selectedSlotIndex, inventory.inventoryHeight - 1] != null)
+            selectedItem = inventory.inventorySlots[selectedSlotIndex, inventory.inventoryHeight - 1].item;
+        else
+            selectedItem = null;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            inventoryShowing = !inventoryShowing;
+        }
+
+        if (Vector2.Distance(transform.position, mousePos) <= playerRange &&
+            Vector2.Distance(transform.position, mousePos) > 0.7f)
+        {
+
+            if (place)
+            {
+                if (selectedItem != null)
+                {
+                    if (selectedItem.itemType == ItemClass.ItemType.block)
+                    {
+                        if(terrainGenerator.CheckTile(selectedItem.tile, mousePos.x, mousePos.y, false))
+                            inventory.Remove(selectedItem);
+                    }
+
+                }
+            }
+        }
+        if(Vector2.Distance(transform.position, mousePos) <= playerRange)
+        {
+            if (hit)
+            {
+                //terrainGenerator.RemoveTile(mousePos.x, mousePos.y);
+                terrainGenerator.BreakTile(mousePos.x, mousePos.y, selectedItem);
+            }
+        }
         mousePos.x = Mathf.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - 0.5f);
         mousePos.y = Mathf.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - 0.5f);
+        inventory.inventoryUI.SetActive(inventoryShowing);
+
+        animator.SetBool("hit", hit || place);
+        animator.SetFloat("Walk", Mathf.Abs(horizontal));
     }
 }
--- Assets/Scripts/TerrainMap/TileClass.cs
+++ Assets/Scripts/TerrainMap/TileClass.cs
@@ -5,8 +5,33 @@ using UnityEngine;
 public class TileClass : ScriptableObject
 {
     public string tileName;
+
+    public TileClass wallVariant;
     //public Sprite tileSprite;
     public Sprite[] tileSprites;
-    public bool inBackground = false;
-    public bool tileDrop = true;
+    public bool inBackground;
+    public TileClass tileDrop;
+    public ItemClass.ToolType toolToBreak;
+
+    public bool isStackable = true;
+    public bool naturallyPlaced = true;
+
+    public static TileClass CreateInstance(TileClass tile, bool isNaturallyPlaced)
+    {
+        var thisTile = ScriptableObject.CreateInstance<TileClass>();
+        thisTile.Init(tile, isNaturallyPlaced);
+        return thisTile;
+    }
+
+    public void Init(TileClass tile, bool isNaturallyPlaced)
+    {
+        tileName = tile.tileName;
+        wallVariant = tile.wallVariant;
+        tileSprites = tile.tileSprites;
+        tileDrop = tile.tileDrop;
+        isStackable = tile.isStackable;
+        toolToBreak = tile.toolToBreak;
+        naturallyPlaced = isNaturallyPlaced;
+        inBackground = tile.inBackground;
+    }
 }
